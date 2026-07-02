7752853ee01a19b908cb84f8d1cbe41a8a01543d
ManuelSG117 <116238982+ManuelSG117@users.noreply.github.com>
2026-07-02 13:21:29 -0600

    feat: add full Astro portfolio project

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from 'astro/config'
+import tailwind from "@astrojs/tailwind"
+
+import robotsTxt from "astro-robots-txt"
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), robotsTxt()],
+  site: 'https://porfolio.dev/',
+  server: {
+    headers: {
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Resource-Policy': 'cross-origin'
+    }
+  }
+})
